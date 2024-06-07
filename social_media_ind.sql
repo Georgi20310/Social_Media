148d01b6f99dc6ba43e8a6929c7ddddf402b4560
deniegotina06 <92509464+deniegotina06@users.noreply.github.com>
2024-06-07 08:29:24 +0300

    Update social_media_ind.sql

--- social_media_ind.sql
+++ social_media_ind.sql
@@ -1,19 +1,19 @@
-CREATE INDEX idx_users_email ON Users(email);
+CREATE INDEX idx_users_email ON users(email);
 
-CREATE INDEX idx_posts_user_date ON Posts(user_id, created_at);
+CREATE INDEX idx_posts_user_date ON posts(user_id, created_at);
 
-CREATE INDEX idx_comments_content_prefix ON Comments((LEFT(content, 10)));
+CREATE INDEX idx_comments_content_prefix ON comments((LEFT(content, 10)));
 
-CREATE INDEX idx_users_username_prefix ON Users((LEFT(username, 5)));
+CREATE INDEX idx_users_username_prefix ON users((LEFT(username, 5)));
 
-CREATE INDEX idx_profiles_user_id ON Profiles(user_id);
+CREATE INDEX idx_profiles_user_id ON profiles(user_id);
 
-CREATE INDEX idx_friends_user_date ON Friends(user_id1, friendship_date);
+CREATE INDEX idx_friends_user_date ON friends(user_id1, friendship_date);
 
-CREATE INDEX idx_posts_content_prefix ON Posts((LEFT(content, 15)));
+CREATE INDEX idx_posts_content_prefix ON posts((LEFT(content, 15)));
 
-CREATE INDEX idx_posts_user_content ON Posts(user_id, content);
+CREATE INDEX idx_posts_user_content ON posts(user_id, content);
 
-CREATE INDEX idx_comments_post_id ON Comments(post_id);
+CREATE INDEX idx_comments_post_id ON comments(post_id);
 
-CREATE INDEX idx_users_email_created ON Users(email, created_at);
+CREATE INDEX idx_users_email_created ON users(email, created_at);
